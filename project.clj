41d14e4ca0a857479fc5d93c9d76a01a9b66cecb
Caio Rulli <caiorulli@gmail.com>
2019-11-13 21:24:59 -0300

    Release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject top-10 "0.1.1"
+(defproject top-10 "0.1.2"
   :description "Choose your top 10 something!"
   :url "http://github.com/caiorulli/top-10"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
